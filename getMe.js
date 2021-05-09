2df20b60e09c338f90e33bf7c72061bb44a3a86b
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-05-09 19:31:26 -0400

    stable??

--- getMe.js
+++ getMe.js
@@ -1,6 +1,7 @@
-const fs = require('fs')
-const SpotifyWebApi = require('spotify-web-api-node');
-const token = "BQCFscR79ADYfWHdFzyP7qw275ednyc-1F-OglAkwf1TWDNgEKwc46Xv9A7wQauCISQscIRlZobK-MTBmdmP-Mvwbxvst7_W_LejjWMJ-e7JiJbfd4uY9Q50nRrtv1rOqli1MwQgZHqh0LklcZx2WgFJc3n0od7sHb0tRPEih4ZjwcS47BmIAreiDXE945WJUGWuRqQPPmEjWUdnb2L0GjV1UwbC3-d-yeGw5qGfNSUvGUUAQ7PLaDEqpLZ5fG1dPHj5AGCmfoX0JGVdX8ehpOuX7zGdBMX62Yw";
+const fs = require("fs");
+const SpotifyWebApi = require("spotify-web-api-node");
+const token =
+  "BQCFscR79ADYfWHdFzyP7qw275ednyc-1F-OglAkwf1TWDNgEKwc46Xv9A7wQauCISQscIRlZobK-MTBmdmP-Mvwbxvst7_W_LejjWMJ-e7JiJbfd4uY9Q50nRrtv1rOqli1MwQgZHqh0LklcZx2WgFJc3n0od7sHb0tRPEih4ZjwcS47BmIAreiDXE945WJUGWuRqQPPmEjWUdnb2L0GjV1UwbC3-d-yeGw5qGfNSUvGUUAQ7PLaDEqpLZ5fG1dPHj5AGCmfoX0JGVdX8ehpOuX7zGdBMX62Yw";
 
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(token);
@@ -11,7 +12,7 @@ function getMyData() {
     const me = await spotifyApi.getMe();
     // console.log(me.body);
     getUserPlaylists(me.body.id);
-  })().catch(e => {
+  })().catch((e) => {
     console.error(e);
   });
 }
@@ -19,20 +20,20 @@ function getMyData() {
 //GET MY PLAYLISTS
 async function getUserPlaylists(userName) {
   const data = await spotifyApi.getUserPlaylists(userName, {
-      offset: 0,
-      limit: 50 //max
-  })
+    offset: 0,
+    limit: 50, //max
+  });
 
-  console.log("---------------+++++++++++++++++++++++++")
-  let playlists = []
+  console.log("---------------+++++++++++++++++++++++++");
+  let playlists = [];
 
   for (let playlist of data.body.items) {
-    console.log(playlist.name + " " + playlist.id)
-    
+    console.log(playlist.name + " " + playlist.id);
+
     let tracks = await getPlaylistTracks(playlist.id, playlist.name);
     console.log(tracks);
 
-    const tracksJSON = { tracks }
+    const tracksJSON = { tracks };
     let data = JSON.stringify(tracksJSON);
     // fs.writeFileSync(playlist.name+'.json', data);
   }
@@ -40,25 +41,24 @@ async function getUserPlaylists(userName) {
 
 //GET SONGS FROM PLAYLIST
 async function getPlaylistTracks(playlistId, playlistName) {
-
   const data = await spotifyApi.getPlaylistTracks(playlistId, {
     offset: 1,
     limit: 100,
-    fields: 'items'
-  })
+    fields: "items",
+  });
 
-//   console.log('The playlist contains these tracks', data.body);
-//   console.log('The playlist contains these tracks: ', data.body.items[0].track);
-  console.log("'" + playlistName + "'" + ' contains these tracks:');
+  //   console.log('The playlist contains these tracks', data.body);
+  //   console.log('The playlist contains these tracks: ', data.body.items[0].track);
+  console.log("'" + playlistName + "'" + " contains these tracks:");
   let tracks = [];
 
   for (let track_obj of data.body.items) {
-    const track = track_obj.track
+    const track = track_obj.track;
     tracks.push(track);
-    console.log(track.name + " : " + track.artists[0].name)
+    console.log(track.name + " : " + track.artists[0].name);
   }
-  
-  console.log("---------------+++++++++++++++++++++++++")
+
+  console.log("---------------+++++++++++++++++++++++++");
   return tracks;
 }
 
--- server.js
+++ server.js
@@ -1,121 +1,135 @@
-const SpotifyWebApi = require('spotify-web-api-node');
-const express = require('./node_modules/express');
-require('dotenv').config();
-const Datastore = require('nedb');
+const SpotifyWebApi = require("spotify-web-api-node");
+const express = require("./node_modules/express");
+require("dotenv").config();
+const Datastore = require("nedb");
 
 const scopes = [
-  'ugc-image-upload',
-  'user-read-playback-state',
-  'user-modify-playback-state',
-  'user-read-currently-playing',
-  'streaming',
-  'app-remote-control',
-  'user-read-email',
-  'user-read-private',
-  'playlist-read-collaborative',
-  'playlist-modify-public',
-  'playlist-read-private',
-  'playlist-modify-private',
-  'user-library-modify',
-  'user-library-read',
-  'user-top-read',
-  'user-read-playback-position',
-  'user-read-recently-played',
-  'user-follow-read',
-  'user-follow-modify'
+  "ugc-image-upload",
+  "user-read-playback-state",
+  "user-modify-playback-state",
+  "user-read-currently-playing",
+  "streaming",
+  "app-remote-control",
+  "user-read-email",
+  "user-read-private",
+  "playlist-read-collaborative",
+  "playlist-modify-public",
+  "playlist-read-private",
+  "playlist-modify-private",
+  "user-library-modify",
+  "user-library-read",
+  "user-top-read",
+  "user-read-playback-position",
+  "user-read-recently-played",
+  "user-follow-read",
+  "user-follow-modify",
 ];
+const app = express();
+const port = 8888;
+console.log("you looking for a boyfriend?");
+app.listen(port, () =>
+  console.log(
+    `HTTP Server up. Now go to http://localhost:${port}/login in your browser.`
+  )
+);
+//serve static pages in public folder
+app.use(express.static("public"));
+app.use(express.json());
 
 const spotifyApi = new SpotifyWebApi({
-  redirectUri: 'http://localhost:8888/callback',
-  clientId: '92fef82c4b9f4b3ca1b3eb08b0001568',
-  clientSecret: process.env.CLIENT_SECRET
+  redirectUri: `http://localhost:${port}/callback`,
+  clientId: "92fef82c4b9f4b3ca1b3eb08b0001568",
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
-const app = express();
+//declare database and load
+const database = new Datastore("myDatabase.db");
+database.loadDatabase();
 
-app.get('/login', (req, res) => {
-  // console.log(spotifyApi.createAuthorizeURL(scopes))
+app.get("/login", (req, res) => {
   res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
 
-app.get('/callback', async (req, res) => {
+console.log("I see that");
+
+//AUTH STUFF
+app.get("/callback", async (req, res) => {
   //rewrite w async/await bc i like it better
   const error = req.query.error;
   const code = req.query.code;
   const state = req.query.state;
 
   if (error) {
-    console.error('Callback Error:', error);
+    console.error("Callback Error:", error);
     res.send(`Callback Error: ${error}`);
     return;
   }
-  
-  try{
+
+  try {
     const data = await spotifyApi.authorizationCodeGrant(code);
 
-    const access_token = data.body['access_token'];
-    const refresh_token = data.body['refresh_token'];
-    const expires_in = data.body['expires_in'];
-    
+    const access_token = data.body["access_token"];
+    const refresh_token = data.body["refresh_token"];
+    const expires_in = data.body["expires_in"];
+
     //STORE ACCESS TOKENS IN CLIENT
     spotifyApi.setAccessToken(access_token);
     spotifyApi.setRefreshToken(refresh_token);
 
-    console.log('access_token:', access_token);
-    console.log('refresh_token:', refresh_token);
+    console.log("access_token:", access_token);
+    console.log("refresh_token:", refresh_token);
 
-    console.log(`Sucessfully retreived access token. Expires in ${expires_in} s.`);
+    console.log(
+      `Sucessfully retreived access token. Expires in ${expires_in} s.`
+    );
     // res.send('Success! You can now close the window.');
-    
+
     //refresh token every half <expires_in>
     setInterval(async () => {
       const data = await spotifyApi.refreshAccessToken();
-      const access_token = data.body['access_token'];
+      const access_token = data.body["access_token"];
 
-      console.log('The access token has been refreshed!');
-      console.log('access_token:', access_token);
+      console.log("The access token has been refreshed!");
+      console.log("access_token:", access_token);
       spotifyApi.setAccessToken(access_token);
-    }, expires_in / 2 * 1000);
+    }, (expires_in / 2) * 1000);
 
-    res.redirect('/');//back to index.html
-    getTopArtistsShort();
-  }catch(error) {
-    console.error('Error getting Tokens:', error);
+    // res.redirect("/top"); //back to index.html
+  } catch (error) {
+    console.error("Error getting Tokens:", error);
     res.send(`Error getting Tokens: ${error}`);
   }
 
-  
+  try {
+    console.log("getting top artists...");
+    const artistData = await spotifyApi.getMyTopArtists({
+      time_range: "short_term",
+      limit: 5,
+    });
+
+    let ids = {};
+    let index = 1;
+    console.log("my top tracks are:");
+    for (artistObj of artistData.body.items) {
+      let id = artistObj.id;
+      let name = artistObj.name;
+      console.log(`${name} id: ${id}`);
+      ids[index++] = id;
+    }
+
+    let store = {
+      date: new Date().toLocaleString("en-US", {
+        timeZone: "America/New_York",
+      }),
+      type: "artists",
+      items: ids,
+    };
+    console.log("+++++++++++++++++++++++++++++++++++");
+    console.log(store);
+    database.insert(store);
+  } catch (error) {
+    console.error("Error getting top artists", error);
+  }
 });
 
-app.listen(8888, () =>
-  console.log('HTTP Server up. Now go to http://localhost:8888/login in your browser.')
-);
-//serve static pages in public folder
-app.use(express.static('public'));
-
-
-/**
- * 
- * @returns array of top artists IDs
- */
-async function getTopArtistsShort(){
-  //get top
-	try {
-		const data = await spotifyApi.getMyTopArtists({ time_range: 'short_term', limit: 5 });
-		
-		console.log('My top artists are:')
-    let artistIDs = [];
-
-    for(let artist_obj of data.body.items){
-      let id = artist_obj.id;
-      
-      artistIDs.push(id);
-      let name = artist_obj.name;
-      console.log(`${name} id: ${id}`);
-    }
-    return artistIDs;
-
-	} catch (error) {
-		console.log('Error getting artists/tracks:', error);
-	}
-}
+console.log("Give me time you know im gonna be that");
