fe0470a1c5cca67907116fefbe2325f4fde6b5ba
ryanwong2000 <56795999+ryanwong2000@users.noreply.github.com>
2021-05-09 19:31:01 -0400

    Revert "stable?"
    This reverts commit c6747fd814a6c55e36e7e8a800bff2d11bff28cc.

--- example.js
+++ example.js
@@ -1,6 +1,6 @@
 /**
  * spotify-web-api-node/examples/tutorial/00-get-access-token.js /
- *
+ * 
  * This example is using the Authorization Code flow.
  *
  * In root directory run
@@ -17,92 +17,90 @@
  */
 
 // THIS IS THE SERVER(index.js)
-const SpotifyWebApi = require("spotify-web-api-node");
-const express = require("./node_modules/express");
-require("dotenv").config();
-const Datastore = require("nedb");
+const SpotifyWebApi = require('spotify-web-api-node');
+const express = require('./node_modules/express');
+require('dotenv').config();
+const Datastore = require('nedb');
 
 const scopes = [
-  "ugc-image-upload",
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "user-read-currently-playing",
-  "streaming",
-  "app-remote-control",
-  "user-read-email",
-  "user-read-private",
-  "playlist-read-collaborative",
-  "playlist-modify-public",
-  "playlist-read-private",
-  "playlist-modify-private",
-  "user-library-modify",
-  "user-library-read",
-  "user-top-read",
-  "user-read-playback-position",
-  "user-read-recently-played",
-  "user-follow-read",
-  "user-follow-modify",
+  'ugc-image-upload',
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+  'streaming',
+  'app-remote-control',
+  'user-read-email',
+  'user-read-private',
+  'playlist-read-collaborative',
+  'playlist-modify-public',
+  'playlist-read-private',
+  'playlist-modify-private',
+  'user-library-modify',
+  'user-library-read',
+  'user-top-read',
+  'user-read-playback-position',
+  'user-read-recently-played',
+  'user-follow-read',
+  'user-follow-modify'
 ];
-
+ 
 const spotifyApi = new SpotifyWebApi({
-  redirectUri: "http://localhost:8888/callback",
-  clientId: "92fef82c4b9f4b3ca1b3eb08b0001568",
-  clientSecret: process.env.CLIENT_SECRET,
+  redirectUri: 'http://localhost:8888/callback',
+  clientId: '92fef82c4b9f4b3ca1b3eb08b0001568',
+  clientSecret: process.env.CLIENT_SECRET
 });
-
+ 
 const app = express();
-
-app.get("/login", (req, res) => {
+ 
+app.get('/login', (req, res) => {
   res.redirect(spotifyApi.createAuthorizeURL(scopes));
 });
-
-app.get("/callback", (req, res) => {
+ 
+app.get('/callback', (req, res) => {
   const error = req.query.error;
   const code = req.query.code;
   const state = req.query.state;
-
+ 
   if (error) {
-    console.error("Callback Error:", error);
+    console.error('Callback Error:', error);
     res.send(`Callback Error: ${error}`);
     return;
   }
-
+ 
   spotifyApi
     .authorizationCodeGrant(code)
-    .then((data) => {
-      const access_token = data.body["access_token"];
-      const refresh_token = data.body["refresh_token"];
-      const expires_in = data.body["expires_in"];
+    .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
       spotifyApi.setAccessToken(access_token);
       spotifyApi.setRefreshToken(refresh_token);
-
-      console.log("access_token:", access_token);
-      console.log("refresh_token:", refresh_token);
-
+ 
+      console.log('access_token:', access_token);
+      console.log('refresh_token:', refresh_token);
+ 
       console.log(
         `Sucessfully retreived access token. Expires in ${expires_in} s.`
       );
-      res.send("Success! You can now close the window.");
-
+      res.send('Success! You can now close the window.');
+ 
       setInterval(async () => {
         const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body["access_token"];
-
-        console.log("The access token has been refreshed!");
-        console.log("access_token:", access_token);
+        const access_token = data.body['access_token'];
+ 
+        console.log('The access token has been refreshed!');
+        console.log('access_token:', access_token);
         spotifyApi.setAccessToken(access_token);
-      }, (expires_in / 2) * 1000);
+      }, expires_in / 2 * 1000);
     })
-    .catch((error) => {
-      console.error("Error getting Tokens:", error);
+    .catch(error => {
+      console.error('Error getting Tokens:', error);
       res.send(`Error getting Tokens: ${error}`);
     });
 });
-
+ 
 app.listen(8888, () =>
-  console.log(
-    "HTTP Server up. Now go to http://localhost:8888/login in your browser."
-  )
+  console.log('HTTP Server up. Now go to http://localhost:8888/login in your browser.')
 );
 
 //*******************GETTING CREATIVE**********************
@@ -113,10 +111,11 @@ function getMyData() {
     const me = await spotifyApi.getMe();
     console.log(me.body);
     // getUserPlaylists(me.body.id);
-  })().catch((e) => {
+  })().catch(e => {
     console.error(e);
   });
 }
 getMyData();
-const database = new Datastore("database.db");
+const database = new Datastore('database.db');
 database.loadDatabase();
+
